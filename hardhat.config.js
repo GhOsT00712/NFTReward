2fce2a55bcb0f065d6eb36ae28bbbf6f3ae99b33
GhOsT <get.piyush007@gmail.com>
2022-07-11 19:13:17 +0000

    added nft contract and config

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "ropsten",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+};
