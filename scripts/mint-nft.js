211caf33a52aa8e3dc7a13a4571ee81380f5680e
GhOsT <get.piyush007@gmail.com>
2022-09-16 10:52:42 +0000

    fix tokenId

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,9 +1,17 @@
 require("dotenv").config()
 const fetch = require("node-fetch")
+const { Alchemy, Network } = require("alchemy-sdk")
+
 // env variables 
 const API_URL = process.env.API_URL;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const config = {
+    apiKey: "N5s7eE3IXH1Udfxs2P2RtRa0G_1SSt-V",
+    network: Network.ETH_RINKEBY,
+};
+const alchemy = new Alchemy(config);
 
 //alchemy web3 instance
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
@@ -13,7 +21,7 @@ const web3 = createAlchemyWeb3(API_URL)
 const contract = require("../artifacts/contracts/nft.sol/NFT.json")
 // console.log(JSON.stringify(contract.abi))
 
-const contractAddress = "0xF994F1Aa906b347F458034e5aBBFaC1d0f28b2CA" //public address of contract
+const contractAddress = "0x10c8CC0369FA0D4a00BC89354212B5d2e074e742" //public address of contract
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
 
 // create transaction
@@ -36,12 +44,19 @@ async function mintNFT(tokenURI) {
         signedTX = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
         hash = await web3.eth.sendSignedTransaction(signedTX.rawTransaction)
         console.log(
-            "The hash of your transaction is: ",
-            hash.transactionHash,
-            "\nCheck Alchemy's Mempool to view the status of your transaction!"
-        )
+            "The hash of your transaction is: ", hash.transactionHash)
         await sleep(8000)
-        getNFTMetadata(nonce);
+
+        //getting latest transaction data
+        const tx = await web3.eth.getTransactionReceipt(hash.transactionHash)
+
+        let logs = tx.logs;
+        // console.log(logs);
+        const topicId = web3.utils.hexToNumber(logs[0].topics[3])
+        console.log("topicId ", topicId);
+        getNFTMetadata(topicId);
+
+
     } catch (err) {
         console.log(err);
     }
@@ -49,7 +64,7 @@ async function mintNFT(tokenURI) {
 
 //mint NFT with nft-metadata.json uploaded in ipsf (pinata)
 console.log(" All looks good ! Minting NFT now.")
-mintNFT("https://gateway.pinata.cloud/ipfs/QmcSvsZ9Suz1PDKt1xgJM82mFq1gx284Z1uCRgTH4cc8jt")
+// mintNFT("https://gateway.pinata.cloud/ipfs/QmcSvsZ9Suz1PDKt1xgJM82mFq1gx284Z1uCRgTH4cc8jt")
 
 // search NFT metadata
 async function getNFTMetadata(tokenId) {
@@ -70,4 +85,27 @@ function sleep(ms) {
         setTimeout(resolve, ms);
     });
 }
+
+
+async function getAllTransactionOnAddress() {
+    // Get all NFTs
+    const nfts = await alchemy.nft.getNftsForOwner(PUBLIC_KEY);
+    // Print NFTs
+    console.log(nfts);
+}
+getAllTransactionOnAddress()
+
+// async function transferNFT() {
+//     try {
+//         console.log(await nftContract.methods.ownerOf(16).call())
+//         // await nftContract.methods.approve(PUBLIC_KEY, 16).call()
+//         // await nftContract.methods.safeTransferFrom(PUBLIC_KEY, "0xE40a221AaEa01aBa3271ebBC4A0d9101Fb668898", 16).call()
+//     }
+//     catch (ex) {
+//         console.log(ex.message)
+//     }
+//     // await nftContract.methods.safeTransferFrom(PUBLIC_KEY, "0xE40a221AaEa01aBa3271ebBC4A0d9101Fb668898", 15).call()
+// }
+
+// console.log("Transffering NFT to new owner")
 
