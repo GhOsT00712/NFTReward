d8d6fe9c62e0dbba31f5581b30e222ad2931789f
GhOsT <get.piyush007@gmail.com>
2022-09-18 10:31:44 +0000

    added express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require("dotenv").config()
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on url http://localhost:${port}`)
+})
