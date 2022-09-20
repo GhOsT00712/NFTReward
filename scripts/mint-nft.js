4b7d0cbf79d8a6ce4afb84630b048314bbb5edee
Tanish Garg <tanishgarg@microsoft.com>
2022-09-20 16:07:56 +0530

    Mint NFT and get User NFT API

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -8,8 +8,8 @@ const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config = {
-    apiKey: "N5s7eE3IXH1Udfxs2P2RtRa0G_1SSt-V",
-    network: Network.ETH_RINKEBY,
+    apiKey: "ntTumht4jK8DDCy7ry10K_nggxN3GpQZ",
+    network: Network.ETH_GOERLI,
 };
 const alchemy = new Alchemy(config);
 
@@ -21,14 +21,14 @@ const web3 = createAlchemyWeb3(API_URL)
 const contract = require("../artifacts/contracts/nft.sol/NFT.json")
 // console.log(JSON.stringify(contract.abi))
 
-const contractAddress = "0x10c8CC0369FA0D4a00BC89354212B5d2e074e742" //public address of contract
+const contractAddress = "0xb7529610fc2d5661a1d07e030e83e41b24dbc583" //public address of contract
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
 
 // create transaction
 async function mintNFT(tokenURI) {
     const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); // get transaction count
 
-    const tx = {
+    var tx = {
         from: PUBLIC_KEY,
         to: contractAddress,
         nonce: nonce,
@@ -48,23 +48,18 @@ async function mintNFT(tokenURI) {
         await sleep(8000)
 
         //getting latest transaction data
-        const tx = await web3.eth.getTransactionReceipt(hash.transactionHash)
+        tx = await web3.eth.getTransactionReceipt(hash.transactionHash)
 
         let logs = tx.logs;
         // console.log(logs);
-        const topicId = web3.utils.hexToNumber(logs[0].topics[3])
-        console.log("topicId ", topicId);
-        getNFTMetadata(topicId);
-
+        const tokenId = web3.utils.hexToNumber(logs[0].topics[3])
+        console.log(await getNFTMetadata(tokenId))
+        return tokenId;
 
     } catch (err) {
         console.log(err);
     }
 }
-
-//mint NFT with nft-metadata.json uploaded in ipsf (pinata)
-console.log(" All looks good ! Minting NFT now.")
-// mintNFT("https://gateway.pinata.cloud/ipfs/QmcSvsZ9Suz1PDKt1xgJM82mFq1gx284Z1uCRgTH4cc8jt")
 
 // search NFT metadata
 async function getNFTMetadata(tokenId) {
@@ -76,7 +71,7 @@ async function getNFTMetadata(tokenId) {
         throw new Error(response.statusText);
 
     const json = await response.json();
-    console.log(json)
+    return json;
 }
 
 //sleep function
@@ -86,26 +81,13 @@ function sleep(ms) {
     });
 }
 
-
-async function getAllTransactionOnAddress() {
-    // Get all NFTs
-    const nfts = await alchemy.nft.getNftsForOwner(PUBLIC_KEY);
-    // Print NFTs
-    console.log(nfts);
+async function getAllTransactionOnAddress(userPublicKey) {
+    // Get all NFTs under a particular user
+    const nfts = await alchemy.nft.getNftsForOwner(userPublicKey);
+    return nfts;
 }
-getAllTransactionOnAddress()
 
-// async function transferNFT() {
-//     try {
-//         console.log(await nftContract.methods.ownerOf(16).call())
-//         // await nftContract.methods.approve(PUBLIC_KEY, 16).call()
-//         // await nftContract.methods.safeTransferFrom(PUBLIC_KEY, "0xE40a221AaEa01aBa3271ebBC4A0d9101Fb668898", 16).call()
-//     }
-//     catch (ex) {
-//         console.log(ex.message)
-//     }
-//     // await nftContract.methods.safeTransferFrom(PUBLIC_KEY, "0xE40a221AaEa01aBa3271ebBC4A0d9101Fb668898", 15).call()
-// }
+module.exports = { mintNFT, getAllTransactionOnAddress };
 
-// console.log("Transffering NFT to new owner")
+
 
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+const nft = require("./scripts/mint-nft.js");
 const express = require('express')
 require("dotenv").config()
+
 const app = express()
 const port = process.env.PORT
+const metaDataHost = process.env.METADATAHOST
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/get/:userAddress', async (req, res) => {
+    const { userAddress } =req.params;
+    res.send(await nft.getAllTransactionOnAddress(userAddress));
+})
+
+app.post('/mint', async (req, res) => {
+    const help = await nft.mintNFT(metaDataHost);
+    res.send('Token Id of the NFT is ' + help)
 })
 
 app.listen(port, () => {
